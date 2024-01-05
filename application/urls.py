5ee1d15c2988a1928deb4d5de3bf113c1052102e
prajyotsancheti177 <prajyotsancheti177@gmail.com>
2024-01-05 17:48:16 +0530

    Added static to index

--- application/urls.py
+++ application/urls.py
@@ -5,12 +5,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('signup', views.signup, name="signup"),
+    path('', views.signup, name="signup"),
+    path('index', views.home, name="home"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
     path('authors-sellers/', views.authors_sellers_page, name="author_sellers_page"),
     path('upload-image/', views.upload_image, name="upload_image"),
     path('uploaded-image/', views.uploaded_images, name="uploaded_images"),
     # path('authors-sellers/', views.authors_sellers_page.as_view(), name='authors_sellers_page'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
